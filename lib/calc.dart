1965b83f71633d795bd463664ed3aa681f4c0ba1
Lucas Bustamante <lucascostabustamante@gmail.com>
2022-06-24 00:09:35 -0300

    complementando funções

--- lib/calc.dart
+++ lib/calc.dart
@@ -4,6 +4,9 @@ int Dig1 = 0;
 int Dig2 = 0;
 String Operation = '';
 bool Test = false;
+bool Virg = false;
+String Memory = '';
+
 
 operation (String ButtonValue){
 
@@ -11,41 +14,76 @@ operation (String ButtonValue){
     Display = '0';
     Receptor = '';
     Test = false;
+    Virg = false;
+    Dig1 = 0;
+    Dig2 = 0;
   }
   else if(ButtonValue == '+' || ButtonValue == '-' || ButtonValue == '%' ||
-  ButtonValue == '+/-' || ButtonValue == '×' || ButtonValue == '÷'){
+      ButtonValue == '×' || ButtonValue == '÷'){
     Operation = ButtonValue;
     Dig1 = int.parse(Receptor);
     Test = true;
     Display = Receptor + Operation;
-    Receptor = Display;
+    Memory = Display;
   }
+
+  else if(Display == '0' && ButtonValue == '0'){
+    Display = Display;
+  }
+
+  //TODO: Criar função para numero negativos
+  else if(ButtonValue == '+/-'){
+
+  }
+//TODO: Corrigir somas com double
+  else if(ButtonValue == ','){
+    if(Virg == false) {
+      Virg = true;
+    if (Display == '0') {
+      Receptor = '0' + ButtonValue;
+      Display = Receptor;
+      print(Display);
+    }else{
+      Receptor += ButtonValue;
+      Display = Receptor;
+    }
+  }
+  }
+
   else if (ButtonValue == '='){
     Receptor = '';
-    if(Operation == '+'){
-      Display = (Dig1 + Dig2).toString();
-    }
-    else if(Operation == '-'){
-      Display = (Dig1 - Dig2).toString();
-    }
-    else if(Operation == '×'){
-      Display = (Dig1 * Dig2).toString();
-    }
-    else if(Operation == '÷'){
-      if(Dig1%Dig2 != 1){
-        Display = (Dig1/Dig2).toStringAsFixed(0);
-      }else {
-        Display = (Dig1 / Dig2).toString();
-      }
+    switch(Operation){
+      case '+':
+        Display = (Dig1 + Dig2).toString();
+        break;
+      case '-':
+        Display = (Dig1 - Dig2).toString();
+        break;
+      case '×':
+        Display = (Dig1 * Dig2).toString();
+        break;
+      case '÷':
+        if(Dig1%Dig2 != 1){
+          Display = (Dig1/Dig2).toStringAsFixed(0);
+        }else {
+          Display = (Dig1 / Dig2).toString();
+        }
     }
   }
-  
+
   else{
     if(Display.length <= 15){
       Receptor += ButtonValue;
       Display = Receptor;
-      Test ? Dig2 = int.parse(ButtonValue) : false;
+      if(Test == true){
+        Receptor = ButtonValue;
+        Display = Memory + Receptor;
+
+        Dig2 = int.parse(ButtonValue);
+      }
+      print(Dig2);
     }
   }
+
 
 }
