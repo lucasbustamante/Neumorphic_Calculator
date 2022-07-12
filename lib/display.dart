98045760f108125f1dd44108e3b3bd79e5e56fa8
Lucas Bustamante <lucascostabustamante@gmail.com>
2022-07-11 23:29:43 -0300

    atualização 11.07

--- lib/display.dart
+++ lib/display.dart
@@ -1,34 +1,23 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic_null_safety/flutter_neumorphic.dart';
 import 'colors.dart';
 
 
-class Display extends StatefulWidget {
-  const Display({Key? key}) : super(key: key);
-
-  @override
-  _DisplayState createState() => _DisplayState();
-}
-
-class _DisplayState extends State<Display> {
+class DisplayCalc extends StatelessWidget {
+  final String display;
+  DisplayCalc(this.display);
 
   @override
   Widget build(BuildContext context) {
-
     var size = MediaQuery.of(context).size.height*0.01;
     return Container(
-      child: Padding(
-        padding: EdgeInsets.all(size),
-        child: Row(mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Text('0',style: TextStyle(fontSize: 80,
-            color: kPrimaryColor, fontWeight: FontWeight.w100),),
-          ],
-        ),
-      ),
-          height: MediaQuery.of(context).size.height*0.38,
-      width: MediaQuery.of(context).size.width,
+      child: Expanded(
+          child: AutoSizeText(display, maxLines: 1,
+          style: TextStyle(fontSize:80,
+          color: kPrimaryColor,
+          fontWeight: FontWeight.w200)),
+    ),
     );
   }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic_null_safety/flutter_neumorphic.dart';
+import 'package:neumorphic_calculator/display.dart';
 
 import 'calc.dart';
 import 'colors.dart';
@@ -57,14 +58,7 @@ class _HomePageState extends State<HomePage> {
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           SizedBox(height: 200),
-          Container(
-            child: Expanded(
-              child: AutoSizeText(Display, maxLines: 1,
-                  style: TextStyle(fontSize:80,
-                      color: kPrimaryColor,
-                      fontWeight: FontWeight.w200)),
-            ),
-          ),
+          DisplayCalc(Display),
           Row(children: [
             Button('AC',ColorNum: kSecondaryColor),
             Button('+/-',ColorNum: kSecondaryColor),
